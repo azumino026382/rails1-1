61a3e93772e7db9298ffff6a329a7be5ba107563
azumino <ohnoasaki@OhnonoMacBook-Pro.local>
2023-10-17 16:28:58 +0900

    バリデーションエラー表示するよう修正

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,12 +1,16 @@
 class Schedule < ApplicationRecord
-  validates :title, :start, :end, presence: true
+  validates :title, presence: { message: "タイトルを入力してください"}
+  validates :start, presence: { message: "開始日を入力してください"}
+  validates :end, presence: { message: "終了日を入力してください"}
   validates :title, length: { maximum: 20 }
   validates :memo, length: { maximum: 500 }
-  validate :start_end_check
-  
-  def start_end_check
-    errors.add(:end, "の日付を正しく記入して下さい。") unless
-    self.start < self.end
-  end 
+  validate :start_check
+
+  def start_check
+    if self.start.present? && self.end < Date.today
+      errors.add(:end, "過去の日付は使えません")
+    end      
+  end
+
 end
 
